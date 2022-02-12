dae17f9e199fab69133b26c3c88ea29a1c8395c3
sarahwylie <sarahannemariawylie@gmail.com>
2022-02-11 20:51:38 -0600

    finishedgit add -A

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,14 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category,{
-  foreignKey: 'category_id'
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
